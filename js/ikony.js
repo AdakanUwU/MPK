0d447312b487aa5ce7dd0edc13e875623c749843
AdakanUwU <adam.slabikowski@gmail.com>
2025-04-30 20:50:48 +0200

    Poprawiono base href

--- js/ikony.js
+++ js/ikony.js
@@ -3,19 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
   
     // Definicja mapowania skrótów na HTML dla ikon
     const icons = {
-      przeg: '<img src="/ikony/przeg.svg" height="20" class="atr" title="Pojazd przegubowy">',
-      niskopodlogowy: '<img src="/ikony/niskopodłogowy.svg" height="20" class="atr" title="Pojazd niskopodłogowy">',
-      ac: '<img src="/ikony/AC.svg" height="20" class="atr" title="Pojazd klimatyzowany">',
-      kamery: '<img src="/ikony/kamera.svg" height="20" class="atr" title="Pojazd monitorowany">',
-      usb: '<img src="/ikony/USB.svg" height="20" class="atr" title="Pojazd wyposażony w ładowarki USB">',
-      wifi: '<img src="/ikony/WIFI.svg" height="20" class="atr" title="Sieć WIFI w pojeździe">',
-      biletomat: '<img src="/ikony/biletomat.svg" height="20" class="atr" title="Biletomat w pojeździe">',
-      biletkier: '<img src="/ikony/bilet-kier.svg" height="20" class="atr" title="Bilety sprzedawane u kierowcy">',
-      ev: '<img src="/ikony/ev.svg" height="20" class="atr" title="Napęd elektryczny">',
-      cng: '<img src="/ikony/cng.svg" height="20" class="atr" title="Napęd CNG">',
-      gps1: '<img src="/ikony/gps1.svg" height="20" class="gps" title="Lokalizacja GPS">',
-      gps2: '<img src="/ikony/gps2.svg" height="20" class="gps" title="Lokalizacja GPS">',
-      tram: '<img src="/ikony/tramwaj.svg" height="20" class="atr" title="Tramwaj">'
+      przeg: '<img src="ikony/przeg.svg" height="20" class="atr" title="Pojazd przegubowy">',
+      niskopodlogowy: '<img src="ikony/niskopodłogowy.svg" height="20" class="atr" title="Pojazd niskopodłogowy">',
+      ac: '<img src="ikony/AC.svg" height="20" class="atr" title="Pojazd klimatyzowany">',
+      kamery: '<img src="ikony/kamera.svg" height="20" class="atr" title="Pojazd monitorowany">',
+      usb: '<img src="ikony/USB.svg" height="20" class="atr" title="Pojazd wyposażony w ładowarki USB">',
+      wifi: '<img src="ikony/WIFI.svg" height="20" class="atr" title="Sieć WIFI w pojeździe">',
+      biletomat: '<img src="ikony/biletomat.svg" height="20" class="atr" title="Biletomat w pojeździe">',
+      biletkier: '<img src="ikony/bilet-kier.svg" height="20" class="atr" title="Bilety sprzedawane u kierowcy">',
+      ev: '<img src="ikony/ev.svg" height="20" class="atr" title="Napęd elektryczny">',
+      cng: '<img src="ikony/cng.svg" height="20" class="atr" title="Napęd CNG">',
+      gps1: '<img src="ikony/gps1.svg" height="20" class="gps" title="Lokalizacja GPS">',
+      gps2: '<img src="ikony/gps2.svg" height="20" class="gps" title="Lokalizacja GPS">',
+      tram: '<img src="ikony/tramwaj.svg" height="20" class="atr" title="Tramwaj">'
     };
   
     // Funkcja do zamiany skrótów na obrazy
