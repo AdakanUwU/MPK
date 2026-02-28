f004de62b43ad40b10e75766c8537f1da4935687
adakanuwu <adam.slabikowski@gmail.com>
2026-02-28 20:34:01 +0100

    Fixex js

--- js/update-history.js
+++ js/update-history.js
@@ -1,3 +1,8 @@
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 let linesData = [];
 let vehiclesData = [];
 let databaseData = [];
