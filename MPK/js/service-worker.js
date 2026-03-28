729f7a9a5d5b426c34dc14f8e992b5188b904841
adakanuwu <adam.slabikowski@gmail.com>
2026-03-28 23:23:08 +0100

    Create service-worker.js

---
+++ MPK/js/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: zainstalowany');
+  event.waitUntil(
+    caches.open('static-v1').then((cache) => cache.addAll(['../index.html', '..css//style.css']))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
