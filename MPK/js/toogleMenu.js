6fca9487bdb4f09ff217764f80d336827d872752
adakanuwu <adam.slabikowski@gmail.com>
2026-04-01 22:43:08 +0200

    Fixed menu

--- MPK/js/toogleMenu.js
+++ MPK/js/toogleMenu.js
@@ -1,4 +1,13 @@
 function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('show');
-    }
+}
+
+function toggleDropdown(event) {
+    event.stopPropagation();
+    document.getElementById('dropdownMenu').classList.toggle('open');
+}
+
+document.addEventListener('click', function() {
+    document.getElementById('dropdownMenu')?.classList.remove('open');
+});
